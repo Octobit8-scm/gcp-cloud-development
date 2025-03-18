97fbca3dc0fc0d8d84e6694e3d4b5ac2e75d3d54
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-03-18 21:08:54 +0530

    Added cloud run

---
+++ terraform/cloudrun.tf
@@ -0,0 +1,65 @@
+data "google_project" "project" {
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "pubsub-tutorial"
+  location = "us-central1"
+
+  deletion_protection = false # set to true to prevent destruction of the resource
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello" # Replace with newly created image gcr.io/<project_id>/pubsub
+    }
+  }
+  depends_on = [google_project_service.cloudrun_api]
+}
+
+resource "google_service_account" "sa" {
+  account_id   = "cloud-run-pubsub-invoker"
+  display_name = "Cloud Run Pub/Sub Invoker"
+}
+
+resource "google_cloud_run_service_iam_binding" "binding" {
+  location = google_cloud_run_v2_service.default.location
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  members  = ["serviceAccount:${google_service_account.sa.email}"]
+}
+
+resource "google_project_service_identity" "pubsub_agent" {
+  provider = google-beta
+  project  = data.google_project.project.project_id
+  service  = "pubsub.googleapis.com"
+}
+
+resource "google_project_iam_binding" "project_token_creator" {
+  project = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  members = ["serviceAccount:${google_project_service_identity.pubsub_agent.email}"]
+}
+
+resource "google_pubsub_topic" "default" {
+  name = "pubsub_topic"
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  name  = "pubsub_subscription"
+  topic = google_pubsub_topic.default.name
+  push_config {
+    push_endpoint = google_cloud_run_v2_service.default.uri
+    oidc_token {
+      service_account_email = google_service_account.sa.email
+    }
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+  depends_on = [google_cloud_run_v2_service.default]
+}
